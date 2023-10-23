eaaee9600b4643396ff7cc67b3ca0a29b3f3c185
qiaoliang <qiaoliang.email@gmail.com>
2023-10-23 22:10:06 +0800

    0-1 done.

---
+++ 0-1-下载第一个STM32程序-控制板载的LED/src/main.c
@@ -0,0 +1,10 @@
+#include "stm32f103xb.h" //
+int main(void){
+    RCC->APB2ENR = 0x00000010; // Enable GPIOC clock
+    GPIOC->CRH = 0x00300000; // Configure PC13 as output
+    //GPIOC->ODR = 0x00002000; // Set PC13 to high, led off
+    GPIOC->ODR = 0x00000000; // Set PC13 to low, led on
+
+    while(1);
+    return 0;
+}
