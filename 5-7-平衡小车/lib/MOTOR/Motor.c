f69bdfb6cfa9cc0515b9e4bd0f72cdae435242e9
qiaoliang <qiaoliang.email@gmail.com>
2023-11-22 15:36:21 +0800

    5-7-平衡小车

---
+++ 5-7-平衡小车/lib/MOTOR/Motor.c
@@ -0,0 +1,71 @@
+#include "stm32f10x.h"                  // Device header
+#include "PWM.h"
+
+/**
+  * 函    数：直流电机初始化
+  * 参    数：无
+  * 返 回 值：无
+  */
+void Motor_Init(void)
+{
+	/*开启时钟*/
+	RCC_APB2PeriphClockCmd(RCC_APB2Periph_GPIOA, ENABLE);		//开启GPIOA的时钟
+    // 左轮
+	GPIO_InitTypeDef GPIO_InitStructure_Left;
+	GPIO_InitStructure_Left.GPIO_Mode = GPIO_Mode_Out_PP;
+	GPIO_InitStructure_Left.GPIO_Pin = GPIO_Pin_4 | GPIO_Pin_5;
+	GPIO_InitStructure_Left.GPIO_Speed = GPIO_Speed_50MHz;
+	GPIO_Init(GPIOA, &GPIO_InitStructure_Left); // 将PA4和PA5引脚初始化为推挽输出
+
+	// 右轮
+	GPIO_InitTypeDef GPIO_InitStructure_Right;
+	GPIO_InitStructure_Right.GPIO_Mode = GPIO_Mode_Out_PP;
+	GPIO_InitStructure_Right.GPIO_Pin = GPIO_Pin_13 | GPIO_Pin_14;
+	GPIO_InitStructure_Right.GPIO_Speed = GPIO_Speed_50MHz;
+	GPIO_Init(GPIOB, &GPIO_InitStructure_Right);
+
+	PWM_Init();													//初始化直流电机的底层PWM
+}
+
+/**
+  * 函    数：直流电机设置速度
+  * 参    数：Speed 要设置的速度，范围：-100~100
+  * 返 回 值：无
+  */
+void Motor_SetSpeed(int8_t Speed)
+{
+	if (Speed >= 0)							//如果设置正转的速度值
+	{
+		left_positive();
+		right_positive();
+		PWM_SetCompare2(Speed); // PWM设置为速度值
+		PWM_SetCompare3(PWM_MAX - Speed); // 这里为什么是7200-PWM，因为高级定时器的某些通道PWM输出反相，示波器测一下就知道了，群公告里也有写
+	}
+	else									//否则，即设置反转的速度值
+	{
+		left_negative();
+		right_negative();
+		PWM_SetCompare2(-Speed); // PWM设置为负的速度值，因为此时速度值为负数，而PWM只能给正数
+		PWM_SetCompare3(PWM_MAX + Speed);
+	}
+}
+
+void left_positive(){
+	GPIO_SetBits(GPIOA, GPIO_Pin_4);   // PA4置高电平
+	GPIO_ResetBits(GPIOA, GPIO_Pin_5); // PA5置低电平，设置方向为正转
+}
+void left_negative(){
+	GPIO_ResetBits(GPIOA, GPIO_Pin_4); // PA4置低电平
+	GPIO_SetBits(GPIOA, GPIO_Pin_5);   // PA5置高电平，设置方向为反转
+}
+void right_positive()
+{
+	GPIO_SetBits(GPIOB, GPIO_Pin_13);
+	GPIO_ResetBits(GPIOB, GPIO_Pin_14);
+}
+
+void right_negative()
+{
+	GPIO_ResetBits(GPIOB, GPIO_Pin_13);
+	GPIO_SetBits(GPIOB, GPIO_Pin_14);
+}
---
+++ 5-7-平衡小车/src/main.c
@@ -0,0 +1,118 @@
+#include "Delay.h"
+#include "sys.h"
+#include "OLED.h"
+#include "mpu6050.h"
+#include "inv_mpu.h"
+#include "inv_mpu_dmp_motion_driver.h"
+#include "Motor.h"
+#include "Encoder.h"
+#include "PWM.h"
+
+extern float pitch, roll, yaw; // 欧拉角测量值
+float zhongzhi = 0;			   // roll理论值（小车平衡时的角度）
+extern int velocity;		   // 速度测量值（编码器脉冲数，非真实速度）
+extern float velocity_sum;	   // 速度积分
+int motor_flag;				   // 电机使能标志：1使能  0失能
+
+// 直立环PD参数:
+float Kp = -420, Ki = 0, Kd = -2000; // 调完速度环后精调
+// float Kp=-420,Ki=0,Kd=-960; //乘0.6
+// float Kp=-700,Ki=0,Kd=-1600;
+// float Kp=0,Ki=0,Kd=0;
+
+// 速度环PI参数:
+float VKp = +190, VKi = 0.95;
+void Show_MPU6050(void)
+{
+	if (mpu_dmp_get_data(&pitch, &roll, &yaw) == 0) // 得到了欧拉角，存在pitch,roll,yaw里面
+	{
+		OLED_ShowAngle(pitch, roll, yaw); // oled显示角度
+	}
+}
+
+void OLED_ShowVelocity(int velocity)
+{
+
+	if (velocity < 0)
+	{
+		OLED_ShowStr(50, 0, "-", 2); // 显示负号
+		velocity = -velocity;
+	}
+	else
+		OLED_ShowStr(50, 6, " ", 2); // 掩盖负号
+	OLED_Shownum2(58, 6, velocity, 2);
+}
+
+int main(void)
+{
+
+
+	Delay_init();         //延时函数初始化
+	OLED_Init();          //oled初始化
+	MPU_Init();		        //MPU6050初始化
+	DMP_Init();           //DMP初始化（DMP用来解算陀螺仪数据）
+	MPU_exti_init();	  // 陀螺仪外部中断初始化（放最后）
+	Motor_Init();		  // 直流电机初始化
+	PWM_Init();
+	Encoder_Init();		  // 初始化电机编码器
+
+	/********************************************************
+					  到此所有硬件准备完毕
+	*********************************************************/
+
+	OLED_ShowStr(0,0,"Pitch: ",2);
+	OLED_ShowStr(0,2,"Roll: ",2);
+	OLED_ShowStr(0,4,"Yaw: ",2);
+	OLED_ShowStr(0,6, "Velo: ", 2);
+	while (1)
+	{
+		// 【1】安全装置：
+		if (roll < -30 || roll > 30) // 小车可能已经倒了
+		{
+			motor_flag = 0;			 // 关闭电机
+			velocity_sum = 0;		 // 速度积分清0
+			TIM_SetCounter(TIM3, 0);  // TIM3 用于 Encoder了.
+			// TIM_SetCounter(TIM2, 0); // TIM2 用于 PWM了.
+		}
+		else
+			motor_flag = 1; // 开启电机
+
+		// 【2】状态显示：
+		OLED_ShowAngle(pitch, roll, yaw); // 显示欧拉角
+		OLED_ShowVelocity(velocity);	   // 显示速度（非真实速度）
+	}
+}
+/////////////////////////////////////////////////////////////////////
+
+//第二种使用方式：用中断（陀螺仪的INT引脚会在读取到欧拉角后输出中断信号）
+//#include "delay.h"
+//#include "sys.h"
+//#include "OLED.h"
+//#include "mpu6050.h"
+//#include "inv_mpu.h"
+//#include "inv_mpu_dmp_motion_driver.h"
+//extern float pitch,roll,yaw; //欧拉角原始数据（在mpuexti.c定义）
+
+//int main(void)
+//{
+//	NVIC_PriorityGroupConfig(NVIC_PriorityGroup_2); //2位抢占优先级，2位响应优先级
+//	delay_init();    //延时函数初始化
+//	OLED_Init();     //oled初始化
+//	MPU_Init();		   //MPU6050初始化
+//	DMP_Init();      //DMP初始化（DMP用来解算陀螺仪数据）
+//
+//
+///********************************************************
+//                  到此所有硬件准备完毕
+//*********************************************************/
+//
+//	OLED_ShowStr(0,0,"Pitch: ",2);
+//	OLED_ShowStr(0,2,"Roll: ",2);
+//	OLED_ShowStr(0,4,"Yaw: ",2);
+//
+//	//陀螺仪角度的读取在外部中断服务函数中进行，此处仅进行oled显示
+//	while(1)
+//	{
+//	  OLED_ShowAngle(pitch, roll, yaw); //oled显示角度
+//	}
+//}
