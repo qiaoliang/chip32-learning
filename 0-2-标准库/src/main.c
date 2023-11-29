dae030ba7b92896644686b006fb7524b558293f6
qiaoliang <qiaoliang.email@gmail.com>
2023-11-29 10:17:32 +0800

    0-2 标准库可以点板载LED

--- 0-2-标准库/src/main.c
+++ 0-2-标准库/src/main.c
@@ -1,17 +1,33 @@
-#include "stm32f10x.h" // Device header
+#include "stm32f10x.h"
 #include "Delay.h"
-#include "LED.h"
-#include "Key.h"
-/**
- * @brief 使用两个开关分别控制两个LED灯.
- *        每按一次, LED 灯状态取反.
- *
- * @return int
- */
+#define RCC_GPIOC RCC_APB2Periph_GPIOC
+#define LEDBUILDIN GPIO_Pin_13
+#define LEDPORT GPIOC
 int main(void)
 {
+	RCC_APB2PeriphClockCmd(RCC_GPIOC, ENABLE);
+
+	GPIO_InitTypeDef GPIO_InitStructure;
+	GPIO_InitStructure.GPIO_Mode = GPIO_Mode_Out_PP;
+	GPIO_InitStructure.GPIO_Pin = LEDBUILDIN;
+	GPIO_InitStructure.GPIO_Speed = GPIO_Speed_50MHz;
+	GPIO_Init(LEDPORT, &GPIO_InitStructure);
 
 	while (1)
 	{
+		GPIO_ResetBits(LEDPORT, LEDBUILDIN);
+		Delay_ms(500);
+		GPIO_SetBits(LEDPORT, LEDBUILDIN);
+		Delay_ms(500);
+
+		GPIO_WriteBit(LEDPORT, LEDBUILDIN, Bit_RESET);
+		Delay_ms(500);
+		GPIO_WriteBit(LEDPORT, LEDBUILDIN, Bit_SET);
+		Delay_ms(500);
+
+		GPIO_WriteBit(LEDPORT, LEDBUILDIN, (BitAction)0);
+		Delay_ms(500);
+		GPIO_WriteBit(LEDPORT, LEDBUILDIN, (BitAction)1);
+		Delay_ms(500);
 	}
 }
