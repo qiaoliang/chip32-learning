74c333c37eae6ee30f3e34a9bba80c263b0a6c43
qiaoliang <qiaoliang.email@gmail.com>
2024-04-28 21:00:43 +0800

    开始使用Keil目录结构。

---
+++ 5-8-标准库LED-keil-structure/src/USER/main.c
@@ -0,0 +1,33 @@
+#include "delay.h"
+#include "sys.h"
+#define RCC_GPIOC RCC_APB2Periph_GPIOC
+#define LEDBUILDIN GPIO_Pin_13
+#define LEDPORT GPIOC
+int main(void)
+{
+	RCC_APB2PeriphClockCmd(RCC_GPIOC, ENABLE);
+
+	GPIO_InitTypeDef GPIO_InitStructure;
+	GPIO_InitStructure.GPIO_Mode = GPIO_Mode_Out_PP;
+	GPIO_InitStructure.GPIO_Pin = LEDBUILDIN;
+	GPIO_InitStructure.GPIO_Speed = GPIO_Speed_50MHz;
+	GPIO_Init(LEDPORT, &GPIO_InitStructure);
+
+
+	while (1)
+	{
+		GPIO_ResetBits(LEDPORT, LEDBUILDIN);
+		Delay_ms(500);
+		GPIO_SetBits(LEDPORT, LEDBUILDIN);
+		Delay_ms(500);
+		GPIO_ResetBits(LEDPORT, LEDBUILDIN);
+		Delay_ms(500);
+		GPIO_SetBits(LEDPORT, LEDBUILDIN);
+		Delay_ms(500);
+		GPIO_ResetBits(LEDPORT, LEDBUILDIN);
+		Delay_ms(1000);
+		GPIO_SetBits(LEDPORT, LEDBUILDIN);
+		Delay_ms(1000);
+	}
+}
+
